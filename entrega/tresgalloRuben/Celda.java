e11087165233f5fa56bb364f2e25c8a490fb574b
Rub�n Tresgallo Balb�n <ruben.tresgallo@unealum.local>
2025-04-02 13:40:02 +0200

    Primer intento

---
+++ entrega/tresgalloRuben/Celda.java
@@ -0,0 +1,17 @@
+
+public class Celda {
+    
+    private String contenido;
+
+    public Celda() {
+        this.contenido = "";
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }            
+}
